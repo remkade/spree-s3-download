6c1b47d1154a4156b27026124d62733209b6d232
Jack Kinsella <jack@hermestechnologies.ie>
2011-01-11 11:01:36 +0000

    updated a RAILS_ROOT to Rails.root

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
 begin
-  options = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV] || {}
+  options = YAML.load_file("#{Rails.root}/config/s3.yml")[RAILS_ENV] || {}
 rescue Errno::ENOENT
   options = ENV.to_hash
 ensure
