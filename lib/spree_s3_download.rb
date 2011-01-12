2b4673cc2a615741402013d6c07156b2f82cfa0d
Jack Kinsella <jack@hermestechnologies.ie>
2011-01-12 09:30:10 +0000

    preventing a rake db:migrate crash due to scope

--- lib/spree_s3_download.rb
+++ lib/spree_s3_download.rb
@@ -1,4 +1,4 @@
-require 'spree_core'   
+require 'spree_core'
 require "right_aws"
 require 'spree_s3_download_hooks'
 
@@ -14,33 +14,37 @@ module SpreeS3Download
   class Engine < Rails::Engine
     config.autoload_paths += %W(#{config.root}/lib)
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? reqire(c) : load(c)
-      end  
-    
-       Product.send(:include, ProductS3Extension)
+       def self.activate
+         Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+           Rails.env.production? ? reqire(c) : load(c)
+         end
 
-        User.class_eval do
-          def purchased_products
-            orders.completed.map{|o| o.line_items.map{|l| l.variant.product}}.flatten
-          end
+         Product.send(:include, ProductS3Extension)
 
-          def s3_products
-            purchased_products.select{|p| !p.s3_products.empty?}
-          end
-        end
+         User.class_eval do
+           def purchased_products
+             orders.completed.map{|o| o.line_items.map{|l| l.variant.product}}.flatten
+           end
 
-        Order.class_eval do
-          scope :completed, includes(:line_items => {:variant => :product}).order('created_at desc')     
-           # :conditions => {:status => Order::Status::PAID}, We're going to trust the user here.
-         
-        end  
-      
- 
-      
-    end
+           def s3_products
+             purchased_products.select{|p| !p.s3_products.empty?}
+           end
+         end
 
-    config.to_prepare &method(:activate).to_proc
-  end
-end
+         Order.class_eval do
+
+           #causes crash on bootup without this hack (no orders table!)
+           begin
+             scope :completed, includes(:line_items => {:variant => :product}).order('created_at desc')
+             # :conditions => {:status => Order::Status::PAID}, We're going to trust the user here.
+           rescue
+           end
+         end
+
+
+
+       end
+
+       config.to_prepare &method(:activate).to_proc
+       end
+       end
